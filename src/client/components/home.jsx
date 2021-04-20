d95fc5df9be74053ef14b66f231a473d3531220f
Alexey Zerkalenkov <a.zerkalenkov@smart-image.eu>
2021-04-20 09:14:07 +0200

    Update home.jsx file.

--- src/client/components/home.jsx
+++ src/client/components/home.jsx
@@ -53,7 +53,7 @@ class Home extends React.Component {
 
         <section styleName="custom.header">
           <h2>
-            <span>Hello from </span>
+            <span>testing electrode with Travis CI</span>
             <a href="https://github.com/electrode-io">
               {"Electrode"}
               <img src={electrodePng} />
